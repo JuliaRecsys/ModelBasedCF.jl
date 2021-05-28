b2394b538d50e215d6940f381d858452de24e318
Filipe Braida <filipebraida@gmail.com>
2021-05-28 14:46:35 -0300

    create first tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,22 +1,37 @@
 using Persa
 using Test
-using DataFrames
+using DatasetsCF
 
-function createdummydatasetone()
-    df = DataFrame()
-    df[!, :user] = [1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 5, 6, 6, 6, 6, 6, 7, 7, 7]
-    df[!, :item] = [1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5, 6, 1, 2, 4, 5, 6, 2, 4, 5]
-    df[!, :rating] = [2.5, 3.5, 3.0, 3.5, 2.5, 3.0, 3, 3.5, 1.5, 5, 3, 3.5, 2.5, 3.0, 3.5, 4.0, 3.5, 3.0, 4.0, 2.5, 4.5, 3.0, 4.0, 2.0, 3.0, 2.0, 3.0, 3.0, 4.0, 5.0, 3.5, 3.0, 4.5, 4.0, 1.0]
-    return df
+using ModelBasedCF
+
+dataset = DatasetsCF.MovieLens()
+
+@testset "Random Model Tests" begin
+    model = ModelBasedCF.RandomModel(dataset)
+    Persa.train!(model, dataset)
+
+    @test !isnan(model[1,1])
 end
 
-function createdummydatasettwo()
-    df = DataFrame()
-    df[!, :user] = [1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 5, 6, 6, 6, 6, 6, 7, 7, 7]
-    df[!, :item] = [1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5, 6, 1, 2, 4, 5, 6, 2, 4, 5]
-    df[!, :rating] = [2, 7, 3, 3, 8, 3, 3, 3, 1, 9, 3, 3, 2, 3, 10, 4, 3, 3, 4, 2, 4, 3, 4, 2, 3, 2, 3, 3, 4, 5, 9, 10, 7, 6, 8]
-    return df
-end
-####
+@testset "Factorization Matrix Models Tests" begin
+    @testset "Baseline Tests" begin
+        model = ModelBasedCF.Baseline(dataset)
+        Persa.train!(model, dataset, max_epochs = 1)
 
-@test true
+        @test !isnan(model[1,1])
+    end
+
+    @testset "RSVD Tests" begin
+        model = ModelBasedCF.RSVD(dataset, 1)
+        Persa.train!(model, dataset, max_epochs = 1)
+
+        @test !isnan(model[1,1])
+    end
+
+    @testset "IRSVD Tests" begin
+        model = ModelBasedCF.IRSVD(dataset, 1)
+        Persa.train!(model, dataset, max_epochs = 1)
+
+        @test !isnan(model[1,1])
+    end
+end
